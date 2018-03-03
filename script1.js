3a60354283f3b72d8cb3d8ff4abfd37dcabe1e19
mohedsahil <36995476+mohedsahil@users.noreply.github.com>
2018-03-03 13:48:08 +0530

    Add files via upload

---
+++ script1.js
@@ -0,0 +1,10 @@
+
+function comm() {
+	var ans = confirm("Rate This App in playstore");
+	if(ans)
+	window.location="https://play.google.com/store/apps/details?id=com.hion.ip&amp;hl=en"
+}
+  
+
+
+  
